8093b24884eaa0dfba63cbb4d938393ab80a089d
Ender <speaker_ender@protonmail.com>
2022-03-26 19:52:52 -0400

    Preparation for next release

--- example/pages/components/scrollContextProvider.tsx
+++ example/pages/components/scrollContextProvider.tsx
@@ -9,16 +9,16 @@ const ScrollContextProviderPage: NextPage = () => {
             description={[
                 'This component is used for handling the scroll state of the window.',
                 'You can use the hook, useScrollContext to get the current scroll position from any child component of ScrollContextProvider.',
-                'Currently it is throttling using a sample rate of 10ms.'
+                'The default sample rate is 10ms.'
             ]}
             parameters={[
                 {
-                    name: "scrollStateInterval",
+                    name: "listenerInterval",
                     type: "number",
                     optional: true
                 },
                 {
-                    name: "scrollCallbackInterval",
+                    name: "stateInterval",
                     type: "number",
                     optional: true
                 },
