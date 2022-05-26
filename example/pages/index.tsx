e834d580b8877056d4148831377517a357182e2b
Ender <speaker_ender@protonmail.com>
2022-05-25 22:07:04 -0400

    Updating landing page

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -6,9 +6,23 @@ import ParagraphComponent from "../components/content/paragraph";
 import {
   InViewComponent,
   ObserverContextProvider,
+  ScrollContextProvider,
 } from "@speaker-ender/react-scrollr";
+import CustomHeader from "../components/scroll/headers/customHeader";
+import ElementScroll from "../components/scroll/elementScroll";
+import ElementInView from "./demos/elementInView";
 
 const Home: NextPage = () => {
+  const parallaxHeaderCallback = (
+    currentScroll?: number,
+    previousScroll?: number
+  ) => {
+    return {
+      transform: `translate3d(0, -${(currentScroll || 0) / 3}px, 0)`,
+      backdropFilter: `blur(${(currentScroll || 0) / 10}px)`,
+    };
+  };
+
   return (
     <StyledPage>
       <ObserverContextProvider
@@ -20,10 +34,18 @@ const Home: NextPage = () => {
         </InViewComponent>
         <InViewComponent>
           <StyledPanel>
-            <ParagraphComponent text="Tools for performant scroll animations" />
+            <ParagraphComponent text="Managing lots of scroll events in your React Site can be difficult to maintain can serverly degrade performance" />
+            <ParagraphComponent text="React Scrollr uses a callback pattern in conjunction with context hooks in order to keep your code clean while also avoiding re-rendering of your components" />
           </StyledPanel>
         </InViewComponent>
       </ObserverContextProvider>
+      <ElementInView />
+      <ScrollContextProvider>
+        <ElementScroll />
+      </ScrollContextProvider>
+      <ScrollContextProvider>
+        <CustomHeader updateHeaderCallback={parallaxHeaderCallback} />
+      </ScrollContextProvider>
     </StyledPage>
   );
 };
